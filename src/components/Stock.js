cf4da6334eea3cca54830bf829f76c5aba362a43
Alex Trotman <alextrotman04@gmail.com>
2023-11-01 15:56:08 -0500

    save

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function Stock() {
+function handleClick(){
+  console.log('Clicked!')
+}
+
+function Stock({name, price}) {
   return (
-    <div>
+    <div onClick={handleClick}>
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{price}</p>
         </div>
       </div>
     </div>
